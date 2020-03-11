a63bca555464f68017d7322125baa90fc95d70b0
vehernan <hveneranda@yahoo.com>
2020-03-11 14:14:04 -0700

    (: 2

--- INTRO2PYTHON/coursera.py
+++ INTRO2PYTHON/coursera.py
@@ -347,5 +347,56 @@ def longest_word(word1, word2, word3):
 	else:
 		word = word3
 	return(word)
+
+def longest_word(word1, word2, word3):
+    	if len(word1) >= len(word2) and len(word1) >= len(word3):
+		word = word1
+	elif len(word1) < len(word2) and len(word2) > len(word3):
+		word = word2
+	else:
+		word = word3
+	return(word)
+
+print(longest_word("chair", "couch", "table"))
+print(longest_word("bed", "bath", "beyond"))
+print(longest_word("laptop", "notebook", "desktop"))
+
+def sum(x, y):
+      return(x+y)
+    print 3, 7 10
+
+#10
+"""
+ The fractional_part function divides the numerator by the denominator,
+ and returns just the fractional part (a number between 0 and 1).
+ Complete the body of the function so that it returns the right number.
+ Note: Since division by 0 produces an error, if the denominator is 0,
+ the function should return 0 instead of attempting the division.
+"""
+
+def fractional_part(numerator, denominator):
+    	# Operate with numerator and denominator to 
+# keep just the fractional part of the quotient
+	return 0
+
+print(fractional_part(5, 5)) # Should be 0
+print(fractional_part(5, 4)) # Should be 0.25
+print(fractional_part(5, 3)) # Should be 0.66...
+print(fractional_part(5, 2)) # Should be 0.5
+print(fractional_part(5, 0)) # Should be 0
+print(fractional_part(0, 5)) # Should be 0
+
+
+
 #WEEK 3
-#LOOPS 
+#LOOPS
+
+
+  attempts(100)
+
+  #output
+  Attempt 1
+  Attempt 2
+  Attempt 3... until 100
+
+  #
